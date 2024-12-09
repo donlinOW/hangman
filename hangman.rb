ab80cf925bb149a18fb71dd37bfdfdd7f941ea95
Don Lin <don.lin@optimalworkshop.com>
2024-12-09 16:20:25 +1300

    setup game and prompt user for input

--- hangman.rb
+++ hangman.rb
@@ -5,3 +5,10 @@ lives = 9
 guessed_letters = []
 display = "_" * word.length
 
+until lives == 0 || display == word
+  puts "Word: #{display.split('').join(' ')}"
+  puts "Lives: #{lives}"
+  puts "Guessed Letters: #{guessed_letters.join(', ')}"
+  puts "Guess a letter: "
+  guess = gets.chomp.downcase
+end
